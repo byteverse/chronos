baba1ab62d247ed44c6eaa97a9991f8be74f4432
chessai <chessai1996@gmail.com>
2017-10-20 12:42:20 -0400

    dist just got nae-nae'd by repl.sh

--- repl.sh
+++ repl.sh
@@ -6,3 +6,7 @@ rm -rf dist
 
 #this is a workaround while cabal-repl is broken (https://github.com/haskell/cabal/issues/4602)
 nix-shell -A chronos.env release.nix --run "runhaskell Setup.hs configure; runhaskell Setup.hs repl lib:chronos"
+
+# dist can cause problems with nix-build (called by nix-shell)
+# so delete this in between builds
+rm -rf dist
