b193bb9ebaa22374a5438b4b667d52cb7a6a9ab9
chessai <chessai1996@gmail.com>
2017-12-19 02:46:03 -0500

    update nix

--- default.nix
+++ default.nix
@@ -9,17 +9,30 @@ let
   pkgs = import nixpkgs { config = {}; };
   inherit (pkgs) haskell;
 
+  filterPredicate = p: type:
+    let path = baseNameOf p; in !(
+       (type == "directory" && path == "dist")
+    || (type == "symlink"   && path == "result")
+    || (type == "directory" && path == ".git")
+    || (type == "symlink"   && pkgs.lib.hasPrefix "result" path)
+    || pkgs.lib.hasSuffix "~" path
+    || pkgs.lib.hasSuffix ".o" path
+    || pkgs.lib.hasSuffix ".so" path
+    || pkgs.lib.hasSuffix ".nix" path);
+
   overrides = haskell.packages.${compiler}.override {
     overrides = self: super:
     with haskell.lib;
-    with { cp = file: (self.callPackage (./nix/haskell + "/${file}") {}); };
+    with { cp = file: (self.callPackage (./nix/haskell + "/${file}") {}); 
+           build = name: path: self.callCabal2nix name (builtins.filterSource filterPredicate path) {}; 
+         };
 
     {
       mkDerivation = args: super.mkDerivation (args // {
         doCheck = pkgs.lib.elem args.pname [ "chronos" ];
         doHaddock = false;
       });
-      chronos        = cp "chronos.nix";
+      chronos = build "chronos" ./.;
     };
   };
 in rec {
